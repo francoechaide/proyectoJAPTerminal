7323399bc0e85edb97e56c8df8d1bdb39eb0b50a
Franco Echaide <echaidefranco@gmail.com>
2023-08-07 20:06:46 -0300

    subida 3

--- js/products.js
+++ js/products.js
@@ -1,32 +1,30 @@
-let currentCategoriesArray = [];
+let currentProductosArray = [];
 let currentSortCriteria = undefined;
-let minCount = undefined;
-let maxCount = undefined;
 
 function setCatID(id) {
 	localStorage.setItem("catID", id);
 	window.location = "product-info.html";
 }
 
-function showCategoriesList() {
+function showProductosList() {
 	let htmlContentToAppend = "";
-	document.getElementById("titulo").innerHTML = `<h2>Categorías</h2>
-        <p class="lead">Verás aquí todos los productos de la categorioa ${currentCategoriesArray.catName}.</p>`;
+	document.getElementById("titulo").innerHTML = `<h2>Productos</h2>
+        <p class="lead">Verás aquí todos los productos de la categoria ${currentProductosArray.catName}.</p>`;
 
-	for (let i = 0; i < currentCategoriesArray.products.length; i++) {
-		let category = currentCategoriesArray.products[i];
+	for (let i = 0; i < currentProductosArray.products.length; i++) {
+		let Producto = currentProductosArray.products[i];
 		htmlContentToAppend += `
-            <div onclick="setCatID(${category.id})" class="list-group-item list-group-item-action cursor-active">
+            <div onclick="setCatID(${Producto.id})" class="list-group-item list-group-item-action cursor-active">
                 <div class="row">
                     <div class="col-3">
-                        <img src="${category.image}" alt="${category.description}" class="img-thumbnail">
+                        <img src="${Producto.image}" alt="${Producto.description}" class="img-thumbnail">
                     </div>
                     <div class="col">
                         <div class="d-flex w-100 justify-content-between">
-                            <h4 class="mb-1">${category.name} - USD ${category.cost} </h4>
-                            <small class="text-muted">${category.soldCount} artículos</small>
+                            <h4  class="mb-1">${Producto.name} - USD ${Producto.cost} </h4>
+                            <small class="text-muted">${Producto.soldCount} artículos</small>
                         </div>
-                        <p class="mb-1">${category.description}</p>
+                        <p class="mb-1">${Producto.description}</p>
                     </div>
                 </div>
             </div>
@@ -39,8 +37,8 @@ function showCategoriesList() {
 document.addEventListener("DOMContentLoaded", function (e) {
 	getJSONData(PRODUCTS_URL + localStorage.catID + ".json").then(function (resultObj) {
 		if (resultObj.status === "ok") {
-			currentCategoriesArray = resultObj.data;
-			showCategoriesList();
+			currentProductosArray = resultObj.data;
+			showProductosList();
 			//sortAndShowCategories(ORDER_ASC_BY_NAME, resultObj.data);
 		}
 	});
